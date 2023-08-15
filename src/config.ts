a4140ca9e6df5b13b1c5e0a8b203fdc27249006f
“Focus-LuisSiliezar” <Luis.siliezar@focusservices.com>
2023-08-14 18:58:58 -0600

    chore: config site

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'La Confe 28';
+export const SITE_DESCRIPTION = 'Welcome to el website de la Confe 28! Aquí podrás encontrar toda la información del evento y realizar tu inscripción para ser parte del evento más chivo del año.';
